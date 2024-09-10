e628c171c3d47f606e932572c7e5d4e2597b5800
jimena blanco <blancojimena.92@gmail.com>
2024-09-10 11:07:23 -0300

    Variables y condicionales

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+
+string nombre = "Jimena";
+int edad = 31;
+double precio = 20.50;
+bool estaNublado = true;
+char inicialApellido = 'B';
+DateTime dateTime = DateTime.Now;
+DateOnly fechaSolo;
+TimeOnly horaSolo;
+
+const string constanteSaludo = "Buen dia";
+
+string saludo;
+
+if (estaNublado)
+{
+    //saludo = "Hola " + nombre + "!. Hoy no es un buen dia";
+    saludo = $"Hola {nombre}!. Hoy no es un buen dia";
+}
+else
+{
+    saludo = "Hola " + nombre + "!. Que lindo dia es hoy!";
+}
+
+
+Console.WriteLine(saludo);
