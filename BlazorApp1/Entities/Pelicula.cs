bd79ede8042a86b75b4270d2f37b100c07d9f30c
jimena blanco <blancojimena.92@gmail.com>
2024-10-23 12:26:53 -0300

    Tarea blazor ABM

---
+++ BlazorApp1/Entities/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp1.Entities
+{
+    public class Pelicula
+    {
+        public string Nombre;
+        public int Valoracion;
+
+        public Pelicula(string nombre, int valoracion)
+        {
+            Nombre = nombre;
+            Valoracion = valoracion;
+        }
+    }
+}
